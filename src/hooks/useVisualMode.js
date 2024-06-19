587f360d5fd583613f607d7750cf8bf05a6cee60
creynolds8 <cameron8.cr@gmail.com>
2024-06-18 20:51:48 -0700

    transition function now passes all tests, allowing modes in history to be replaced if second argument is true

--- src/hooks/useVisualMode.js
+++ src/hooks/useVisualMode.js
@@ -1,17 +1,21 @@
 import { useState } from "react";
 
 export default function useVisualMode(initial) {
-  const [ mode, setMode ] = useState(initial)
-  const [ history, setHistory ] = useState([initial])
-  
-  function transition(newMode) {
+  const [mode, setMode] = useState(initial);
+  const [history, setHistory] = useState([initial]);
+
+  function transition(newMode, replace = false) {
     setMode(newMode);
-    setHistory(prev => [...prev, newMode])
+    setHistory((prev) => {
+      if (replace) {
+        return [...prev.slice(0, prev.length - 1), newMode];
+      } else return [...prev, newMode];
+    });
   }
-  
+
   function back() {
     if (history.length > 1) {
-      setHistory(prev => [...prev.slice(0, prev.length - 1)]) 
+      setHistory((prev) => [...prev.slice(0, prev.length - 1)]);
     }
   }
 
