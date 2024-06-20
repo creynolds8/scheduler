bab7e3cf225a1e614cfacc218d0f3b50b5082e9a
creynolds8 <cameron8.cr@gmail.com>
2024-06-19 18:01:22 -0700

    create simple test for appointment component

--- src/components/__tests__/Appointment.test.js
+++ src/components/__tests__/Appointment.test.js
@@ -4,3 +4,8 @@ import { render } from "@testing-library/react";
 
 import Application from "components/Application";
 
+describe("Appointment", () => {
+  it("renders without crashing", () => {
+    render(<Application />)
+  })
+})
