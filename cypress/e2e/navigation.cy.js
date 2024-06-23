cd6ce666e2f4468b67b1f179448e16593c35093b
creynolds8 <cameron8.cr@gmail.com>
2024-06-23 13:57:17 -0700

    add first e2e test for basic site navigation

--- cypress/e2e/navigation.cy.js
+++ cypress/e2e/navigation.cy.js
@@ -1,5 +1,11 @@
-describe('template spec', () => {
-  it('passes', () => {
-    cy.visit('https://example.cypress.io')
-  })
-})
+describe("Navigation", () => {
+  it("should visit root", () => {
+    cy.visit("/");
+  });
+  it("should navigate to Tuesday", () => {
+    cy.visit("/");
+    cy.contains("[data-testid=day]", "Tuesday")
+      .click()
+      .should("have.class", "day-list__item--selected");
+  });
+});
