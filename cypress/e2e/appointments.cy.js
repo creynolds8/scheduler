06857e4c8c1ab8f74a75e145fdfcc16ab816f5be
creynolds8 <cameron8.cr@gmail.com>
2024-06-23 21:34:59 -0700

    add test to check edit appointment functionality

--- cypress/e2e/appointments.cy.js
+++ cypress/e2e/appointments.cy.js
@@ -1,21 +1,30 @@
 describe("Appointments", () => {
   beforeEach(() => {
-    cy.request("GET", "/api/debug/reset")
+    cy.request("GET", "/api/debug/reset");
     cy.visit("/");
     cy.contains("Monday");
-  })
+  });
 
   it("should book an apoointment", () => {
     cy.get("[alt=Add]")
     .first()
     .click();
     cy.get("[data-testid=student-name-input]")
-    .type("Lydia Miller-Jones")
+    .type("Lydia Miller-Jones");
     cy.get("[alt='Sylvia Palmer']")
+    .click();
+    cy.contains("Save")
+    .click();
+    cy.contains(".appointment__card--show", "Sylvia Palmer");
+    cy.contains(".appointment__card--show", "Lydia Miller-Jones");
+  });
+  it("should edit an interview", () => {
+    cy.get("[alt=Edit]")
+    .click({force: true})
+    cy.get("[alt='Tori Malcolm']")
     .click()
     cy.contains("Save")
     .click()
-    cy.contains(".appointment__card--show", "Sylvia Palmer");
-    cy.contains(".appointment__card--show", "Lydia Miller-Jones");
+    cy.contains(".appointment__card--show", "Tori Malcolm");
   });
 });
