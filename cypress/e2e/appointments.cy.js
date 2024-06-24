7dc3169f573a64bb3a6619c82a0aa4e771ca6911
creynolds8 <cameron8.cr@gmail.com>
2024-06-23 18:00:29 -0700

    complete bookingtest and refactor to pull every test steps to before each block

--- cypress/e2e/appointments.cy.js
+++ cypress/e2e/appointments.cy.js
@@ -1,11 +1,21 @@
 describe("Appointments", () => {
-  it("should book an apoointment", () => {
+  beforeEach(() => {
+    cy.request("GET", "/api/debug/reset")
     cy.visit("/");
     cy.contains("Monday");
+  })
+
+  it("should book an apoointment", () => {
     cy.get("[alt=Add]")
     .first()
     .click();
     cy.get("[data-testid=student-name-input]")
     .type("Lydia Miller-Jones")
+    cy.get("[alt='Sylvia Palmer']")
+    .click()
+    cy.contains("Save")
+    .click()
+    cy.contains(".appointment__card--show", "Sylvia Palmer");
+    cy.contains(".appointment__card--show", "Lydia Miller-Jones");
   });
 });
